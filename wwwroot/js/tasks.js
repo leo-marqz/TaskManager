e7e069657c8535729207dd3e7b5d580d95e93d3f
LeoMarqz <leomarqz2020@gmail.com>
2022-11-13 07:09:46 -0600

    working with tasks - in process

--- wwwroot/js/tasks.js
+++ wwwroot/js/tasks.js
@@ -1,6 +1,15 @@
 ﻿
-function AddTask() {
-    tasksViewModel.tasks.push({ tite: "nueva tarea" })
+export class Task {
+
+    #taskViewModel = null;
+
+    constructor(taskViewModel) {
+        console.log(typeof taskViewModel);
+        this.#taskViewModel = taskViewModel;
+    }
+
+    AddTask(e) {
+        console.log(e.target);
+        this.#taskViewModel.tasks.push({ title: "tarea agregada con clase Task" });
+    }
 }
-
-
